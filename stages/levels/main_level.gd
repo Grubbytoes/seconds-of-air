9a69ee3a7602d9de784370c790e36461e99f93aa
hughie <=>
2025-11-13 12:45:26 +0000

    Holy goddamn shit

--- stages/levels/main_level.gd
+++ stages/levels/main_level.gd
@@ -4,18 +4,35 @@ extends Node2D
 
 
 func _ready():
-	for i in range(24):
-		var row: Array[Vector2i] = []
-		row.resize(24)
+	build_chunk()
 
-		for j in range(2):
-			row[j] = Vector2i(i, j)
-		
-		ter.place_solid_terrain(row)
 
-	for i in range(24):
-		for j in range(2, 4):
-			ter.place_destructible_tile(Vector2i(i, j))
+func build_chunk():
+	var json_chunk := ChunkReader.read_chunk_file("test_chunk.json")
+	var map = json_chunk.data["map"]
+	var terrain_count = json_chunk.data["terrain_count"]
+	var terrain_array: Array[Vector2i] = []
+	var y = 0
+	var x = 0
+	terrain_array.resize(terrain_count)
+
+	for row in map:
+		for c in row:
+			if c == 'x':
+				ter.place_destructible_tile(Vector2i(x, y))
+			elif c == 's':
+				terrain_count -= 1
+				terrain_array[terrain_count] = Vector2i(x, y)
+			x += 1
+
+		y += 1
+		x = 0
+		print(row)
+	
+	ter.place_solid_terrain(terrain_array)
+
+	
+
 		
 		
 	
