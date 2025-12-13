1552330b4140d84761e816bbbb23d38409e362f4
Hugo <hugoaaabbott@gmail.com>
2025-12-13 21:24:01 +0000

    Add score screen and score cache. now technically a functioning game. Thank god.

--- game.gd
+++ game.gd
@@ -4,9 +4,11 @@ extends Node2D
 static var _loaded_stages := {
 	"game stage" = load("res://stages/levels/main_level.tscn"),
 	"main menu" = load("res://stages/menus/main/main_menu.tscn"),
+	"results menu" = load("res://stages/menus/results_display/results_display.tscn")
 }
 
 var current_stage: Stage
+var results_cache: SessionResults
 
 @onready var ui_layer: CanvasLayer =$UILayer
 
@@ -29,6 +31,22 @@ func load_stage(stage_key: String):
 		current_stage.queue_free()
 	
 	# setting up new stage
-	add_child(new_stage)
 	new_stage.attach_game(self)
 	current_stage = new_stage
+	add_child(new_stage)
+
+
+func cache_results(r: SessionResults):
+	results_cache = r
+
+
+func get_cached_results() -> SessionResults:
+	return results_cache
+
+
+func store_results() -> bool:
+	if results_cache == null:
+		return false
+	
+	# todo
+	return true
--- stages/levels/main_level.gd
+++ stages/levels/main_level.gd
@@ -3,3 +3,5 @@ extends Stage
 
 func on_session_end(results: SessionResults):
     queue_next_stage("main menu")
+    get_game().cache_results(results)
+    queue_next_stage("results menu")
